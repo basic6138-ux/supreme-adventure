2258ba32ea6d19b4960f0ec9ea1f76bc00e56e87
basic6138-ux <basic6138@gmail.com>
2026-04-06 16:44:12 +0000

    Remove Tailwind CDN and use Vite-compiled CSS in production

--- Apartment_BoookingSystem/laravel_app/resources/views/layouts/app.blade.php
+++ Apartment_BoookingSystem/laravel_app/resources/views/layouts/app.blade.php
@@ -4,10 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Apartment Booking System</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    @if(file_exists(public_path('build/manifest.json')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 text-gray-900">
     <header class="bg-white shadow">
